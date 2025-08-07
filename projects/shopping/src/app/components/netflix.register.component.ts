1b6ab30b1d15627118c38069e94650eca5616ef7
Sakshi Jogdand <sakshijog7774@gmail.com>
2025-08-07 17:07:53 +0530

    add

--- projects/shopping/src/app/components/netflix.register.component.ts
+++ projects/shopping/src/app/components/netflix.register.component.ts
@@ -1,5 +1,19 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'app-register'
+    selector:'app-register',
+    template:`
+    <div class='registet-container'>
+    <p>Ready to watch enter your email </p> 
+    <div>
+    <input type='email' placeholder="email">
+    <button> 
+    Get stated >
+    </button>
+    </div>
+    </div>`,
+    styles:["p{color:red; text-align:center}"]
 })
+export class NetflixRegister{
+
+}
