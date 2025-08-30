63e66772ebd91755d0cba149c5fe5b76ed5bea6b
Sakshi Jogdand <sakshijog7774@gmail.com>
2025-08-30 18:22:10 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  submit(){
-    console.log("submit");
-  }
+
+// counter app start
+//  count=0
+
+//  handleCounter(val:string){
+//   if(val=='minus'){
+//     this.count=this.count-1;
+//   }
+//   else if(val=='plus'){
+//     this.count=this.count+1;
+//   }
+//   else{
+//      this.count=0;
+//   }
+//  }
+// counter app end
+
+
+//event 
+handleEvent(event:any){
+  console.log("function called",event);
+  
+}
+
 }
